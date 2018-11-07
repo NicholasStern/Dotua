3eb36e2e693702df175a305b0d092f5a9cc57d25
zwehrwein <wehrwein@fas.harvard.edu>
2018-11-07 14:31:47 -0500

    Fixed newton_test with different object creator

--- autodiff/examples/newton_demo.py
+++ autodiff/examples/newton_demo.py
@@ -1,43 +1,43 @@
 def NewtonsMethod(func, x0, tol=1e-5,maxiters=100):
     '''
     Computes the roots of func through iterative guesses until change is below tolerance.
-    
+
     Takes in a function (func), an initial guess (x0), an error tolerance (tol), and a maximum number of iterations (maxiters).
-    
+
     func must be composed of 'AutoDiff.Operators' structures.
-    
+
     Example Usage
     -------------
-    
+
         $ import autodiff as ad
         $ x = ad.create_scalar(0)
         $ from autodiff.operators import Operator as op
         $ func = op.sin(x)
         $ NewtonsMethod(y,0)
-        
+
         >>> 1
 
     '''
     xn = x0
 
     for i in range(maxiters):
-        
+
         #Calculate y at this step.
-        y = f(xn)
-        ##y = func.eval(xn)[0]
+        #y = f(xn) from toy example
+        y = func.eval(xn)[0]
         #Calculate derivative at this step
-        dy_dx = df(xn)
-        ##dy_dx = func.eval(xn)[1]
+        #dy_dx = df(xn)
+        dy_dx = func.eval(xn)[1]
 
         #If y reaches tolerance, stop
         if abs(y) < tol:
             return(xn)
             break
-        
+
         else:
             #Compute Newton Step
             x_next = y / dy_dx
             #Update X
             xn = xn + x_next
-            
+
             return(xn)
