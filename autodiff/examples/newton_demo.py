2bf26ccef1f80312a206d1b3b56e0cc6c80b7989
zwehrwein <wehrwein@fas.harvard.edu>
2018-11-07 18:37:37 -0500

    Fixed newton tests

--- autodiff/examples/newton_demo.py
+++ autodiff/examples/newton_demo.py
@@ -1,33 +1,36 @@
-def NewtonsMethod(func, x0, tol=1e-5,maxiters=100):
+def NewtonsMethod(func, x0, tol=1e-15,maxiters=1000):
     '''
     Computes the roots of func through iterative guesses until change is below tolerance.
 
-    Takes in a function (func), an initial guess (x0), an error tolerance (tol), and a maximum number of iterations (maxiters).
+    Takes in a function (func), a point at which to estimate (x0),
+        an error tolerance (tol), and a maximum number of iterations (maxiters).
 
-    func must be composed of 'AutoDiff.Operators' structures.
+    func must be composed of 'AutoDiff.Operator' operations and AutoDiff.Scalar structures.
 
-    Example Usage
-    -------------
+    Example usage:
 
-        $ import autodiff as ad
-        $ x = ad.create_scalar(0)
-        $ from autodiff.operators import Operator as op
-        $ func = op.sin(x)
-        $ NewtonsMethod(y,0)
 
-        >>> 1
+    #f(x) = sin(x)
+
+    def func(x):
+        return AutoDiff.Operator.sin(Scalar(x))
+
+    NewtonsMethod(func,0)
+    >>> 0
+
+
 
     '''
-    xn = x0
+    #from ..nodes.scalar import Scalar
+    xn = x0 #ad.AutoDiff.create_scalar(vals=[0])[0]
 
     for i in range(maxiters):
 
-        #Calculate y at this step.
-        #y = f(xn) from toy example
-        y = func.eval(xn)[0]
-        #Calculate derivative at this step
-        #dy_dx = df(xn)
-        dy_dx = func.eval(xn)[1]
+        ####Calculate y at this step.
+        y = func(xn)._val
+
+        #### Calculate derivative at this step
+        dy_dx = func(xn)._der
 
         #If y reaches tolerance, stop
         if abs(y) < tol:
@@ -40,4 +43,4 @@ def NewtonsMethod(func, x0, tol=1e-5,maxiters=100):
             #Update X
             xn = xn + x_next
 
-            return(xn)
+        return(xn)
--- autodiff/tests/test_newton.py
+++ autodiff/tests/test_newton.py
@@ -1,11 +1,16 @@
 from ..operators.operator import Operator as op
+from .. import autodiff as ad
 from ..nodes.scalar import Scalar
 from ..examples import newton_demo as newton
 import numpy as np
+from scipy import optimize
+from math import isclose
 
-#define example function using ad objects
-#x = Scalar(0,1)
-#func = op.sin(x)
+def func(x):
+    return op.sin(Scalar(x))
 
-#def test_newton_method():
-#    assert newton.NewtonsMethod(func,0) == np.cos(0)
+def f(x):
+    return np.sin(x)
+
+def test_newton_method():
+    assert isclose(newton.NewtonsMethod(func,0), optimize.newton(f,0),abs_tol=1e-10)
