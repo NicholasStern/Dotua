630de8deb3d8254c3faaf132f95420c61625dbc3
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-11-08 13:45:27 -0500

    Updated create_vector method in autodiff

--- autodiff/autodiff.py
+++ autodiff/autodiff.py
@@ -1,4 +1,5 @@
 import autodiff.nodes.scalar as scalar
+import autodiff.nodes.vector as vector
 
 
 class AutoDiff():
@@ -23,4 +24,39 @@ class AutoDiff():
         # Initials the jacobians for the scalars
         for var in vars:
             var.init_jacobian(vars)
+        return vars
+
+    @staticmethod
+    def create_vector(vals, num=1):
+        '''
+
+        INPUTS
+        ======
+        vals: list of lists of floats, compulsory
+            Value of the list of Vector variables
+        num: int, compulsory, default value is 1
+            Number of vector variables in the list
+        RETURNS
+        ========
+        A list of Vector variables
+
+        NOTES
+        =====
+        PRE:
+            - the length of vals sould be num
+        POST: 
+            returns a list of vector variables with value defined in vals
+        '''
+        if(num > len(vals)):
+            print('You want to create a list of {} vectors'.format(num))
+            print('But you only put values for the first {} vectors'.format(len(vals)))
+        elif(num < len(vals)):
+            print('You want to create a list of {} vectors'.format(num))
+            print('But you only put values for {} vectors'.format(len(vals)))
+        vars = [None] * num
+        for i in range(num):
+            try:
+                vars[i] = vector.Vector(vals[i])
+            except Exception:
+                vars[i] = vector.Vector([0])
         return vars
