9498d570268e7484cd0f5faee580defdf3ab9841
Vincent Viego <veviego@college.harvard.edu>
2018-11-08 20:31:18 -0500

    Finished scalar implementation with the completion of power, and added appropriate tests

--- autodiff/tests/test_scalar.py
+++ autodiff/tests/test_scalar.py
@@ -1,4 +1,5 @@
 from ..nodes.scalar import Scalar
+import numpy as np
 
 '''
 Initialize local variables for testing. Since these tests need to be
@@ -21,8 +22,14 @@ f_6 = y * x
 f_7 = x / y
 f_8 = y / x
 
+# Slightly more complicated functions
 g_1 = 10 * x + y / 2 + 1000
 g_2 = -2 * x * x - 1 / y
+
+# Exponential functions
+h_1 = x ** 2
+h_2 = 2 ** x
+h_3 = x ** y
 
 
 def test_jacobian():
@@ -50,6 +57,10 @@ def test_jacobian():
     assert(g_1.eval()[1] == {x: 10, y: 1/2})
     assert(g_2.eval()[1] == {x: -4, y: 1/4})
 
+    # Test jacobians for exponentials and deg > 1 polynomials
+    assert(h_1.eval()[1] == {x: 2 * a, y: 0})
+    assert(h_2.eval()[1] == {x: (2 ** a) * np.log(2), y: 0})
+
 
 def test_add():
     assert(f_1.eval()[0] == a + b)
@@ -71,10 +82,16 @@ def test_multiply():
     # Directly check commutativity
     assert(f_5.eval() == f_6.eval())
 
+
 def test_divide():
     assert(f_7.eval()[0] == a / b)
     assert(f_8.eval()[0] == b / a)
 
+
+def test_power():
+    assert(h_1.eval()[0] == a ** 2)
+    assert(h_2.eval()[0] == 2 ** a)
+    assert(h_3.eval()[0] == a ** b)
 
 def test_other():
     assert(g_1.eval()[0] == 10 * a + b / 2 + 1000)
