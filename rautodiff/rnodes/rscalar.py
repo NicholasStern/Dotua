2d723a22f17f107b374b26ad2a1d10f00f77f8c5
Vincent Viego <veviego@college.harvard.edu>
2018-11-30 00:51:50 -0500

    Completed scalar for reverse mode implementation and tests

--- rautodiff/rnodes/rscalar.py
+++ rautodiff/rnodes/rscalar.py
@@ -1,24 +1,29 @@
 from .rnode import rNode
 import numpy as np
 
+
 class rScalar(rNode):
     def __init__(self, val):
         self.val = val
         self.parents = []
         self.grad_val = None
 
-
     def gradient(self):
         if self.grad_val is None:
             self.grad_val = 0
-            for par, val in self.parents:
-                self.grad_val += par.gradient() * val
+            for parent, val in self.parents:
+                self.grad_val += parent.gradient() * val
         return self.grad_val
 
     def __add__(self, other):
-        new_parent = Scalar(self.val + other.val)
-        self.parents.append((new_parent, 1))
-        other.parents.append((new_parent, 1))
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val += other.val
+            self.parents.append((new_parent, 1))
+            other.parents.append((new_parent, 1))
+        except AttributeError:
+            new_parent.val += other
+            self.parents.append((new_parent, 1))
         return new_parent
 
     def __radd__(self, other):
@@ -31,9 +36,14 @@ class rScalar(rNode):
         return -self + other
 
     def __mul__(self, other):
-        new_parent = Scalar(self.val * other.val)
-        self.parents.append((new_parent, other.val))
-        other.parents.append((new_parent, self.val))
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val *= other.val
+            self.parents.append((new_parent, other.val))
+            other.parents.append((new_parent, self.val))
+        except AttributeError:
+            new_parent.val *= other
+            self.parents.append((new_parent, other))
         return new_parent
 
     def __rmul__(self, other):
@@ -43,25 +53,64 @@ class rScalar(rNode):
         '''
         Need to calculate @self / @other
         '''
-        pass
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val /= other.val
+            self.parents.append((new_parent, 1 / other.val))
+            other.parents.append((new_parent, -self.val / (other.val ** 2)))
+        except AttributeError:
+            new_parent.val /= other
+            self.parents.append((new_parent, 1 / other))
+        return new_parent
 
     def __rtruediv__(self, other):
         '''
         Need to calculate @other / @self
         '''
-        pass
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val = other.val / self.val
+            self.parents.append((new_parent, -other.val / (self.val ** 2)))
+            other.parents.append((new_parent, 1 / self.val))
+        except AttributeError:
+            new_parent.val = other / new_parent.val
+            self.parents.append((new_parent, -other / (self.val ** 2)))
+        return new_parent
 
     def __pow__(self, other):
         '''
         Need to calculate @self ** @other
         '''
-        pass
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val **= other.val
+            self.parents.append((new_parent,
+                                other.val * self.val ** (other.val - 1)))
+            other.parents.append((new_parent,
+                                 self.val ** other.val * np.log(self.val)))
+        except AttributeError:
+            new_parent.val **= other
+            self.parents.append((new_parent, other * self.val ** (other - 1)))
+        return new_parent
 
     def __rpow__(self, other):
         '''
         Need to calculate @other ** @self
         '''
-        pass
+        new_parent = rScalar(self.val)
+        try:
+            new_parent.val = other.val ** self.val
+            self.parents.append((new_parent),
+                                other.val ** self.val * np.log(other.val))
+            other.parents.append((new_parent,
+                                 self.val * other.val ** (self.val - 1)))
+        except AttributeError:
+            new_parent.val = other ** self.val
+            self.parents.append((new_parent,
+                                other ** self.val * np.log(other)))
+        return new_parent
 
     def __neg__(self):
-        pass
+        new_parent = rScalar(-self.val)
+        self.parents.append((new_parent, -1))
+        return new_parent
