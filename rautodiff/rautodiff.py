560cf9051025104630986df3aad6862a74543f9e
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-12-04 03:58:08 -0500

    Updated rautodiff

--- rautodiff/rautodiff.py
+++ rautodiff/rautodiff.py
@@ -12,11 +12,24 @@ class rAutoDiff():
         with the values initialized to the user defined values or all 0
         by default
         '''
+        try:
+        	rscalars = [None] * len(vals)
+        	for i in range(len(vals)):
+        		rscalars[i] = rScalar(vals[i])
+        	return rscalars
+        except TypeError:
+        	rscalar = rScalar(vals)
+        	return rscalar
 
-        pass
-
-    def reset_universe(self):
-        pass
+    def reset_universe(self, var):
+    	try:
+    		for i in range(len(var)):
+    			var[i].parents = []
+    			var[i].grad_val = None
+    	except TypeError:
+    		var.parents = []
+    		var.grad_val = None
 
     def partial(self, func, var):
-        pass
+    	func.grad_val = 1
+    	return var.gradient()
