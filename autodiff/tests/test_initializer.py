40a1ce08f968b39bbe51e952e9cb489e47b3190d
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-12-08 16:48:18 -0500

    Updated test for autodiff

--- autodiff/tests/test_initializer.py
+++ autodiff/tests/test_initializer.py
@@ -11,27 +11,9 @@ def test_single_variable():
     assert(x.eval() == (5, {x: 1}))
 
 
-
-
-
-
-
-
-
 # Test initializing multiple variables at once
 def test_multiple_variables():
     x, y, z = tuple(AutoDiff.create_scalar([1, 2, 3]))
     assert(x.eval() == (1, {x: 1, y: 0, z: 0}))
     assert(y.eval() == (2, {x: 0, y: 1, z: 0}))
     assert(z.eval() == (3, {x: 0, y: 0, z: 1}))
-
-# Test creating a single vector
-def test_single_vector():
-	x = AutoDiff.create_vector([5])
-	assert x._val == 5
-
-# Test creating multiple vectors
-def test_multiple_vectors():
-	x, y = AutoDiff.create_vector([1, 2])
-	assert x._val == 1
-	assert y._val == 2
