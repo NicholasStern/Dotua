7412c8869e0cb772ab206b6ef6caa8b25c8f17a3
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-12-08 17:19:48 -0500

    Updated test for vector

--- autodiff/tests/test_vector.py
+++ autodiff/tests/test_vector.py
@@ -27,23 +27,24 @@ f_9 = 3 * x + y * 2 + x * y
 f_10 = y / 3 + 2 / x + y / x
 f_11 = z + x
 f_12 = z + a
-f_13 = y + z
+f_13 = f_11 + f_12
 f_14 = b - 1
-f_15 = c - b
-f_16 = c - d
+f_15 = f_11 - f_12
+f_16 = c - f_11
 f_17 = f_11 * 2
+
 
 def test_add():
 	assert(f_1.eval() == (3, [1,1]))
 	assert(f_11.eval() == [3,3])
 	assert(f_12.eval() == [2,1])
-	assert(f_13.eval() == [2,2])
+	assert(f_13.eval() == [5,4])
 
 def test_sub():
 	assert(f_2.eval() == (-6, [1,-2]))
 	assert(f_14.eval() == [0,0])
-	assert(f_15.eval() == [0,-1])
-	assert(f_16.eval() == [-1,-2])
+	assert(f_15.eval() == [1,2])
+	assert(f_16.eval() == [-2,-3])
 
 def test_mul():
 	assert(f_3.eval() == (-3, [-3,0]))
