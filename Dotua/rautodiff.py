b7f995e4e0ccf5d39f750ba1fe0292b8be268dfe
Vincent Viego <veviego@college.harvard.edu>
2018-12-11 15:15:16 -0500

    Minor renaming of method names

--- Dotua/rautodiff.py
+++ Dotua/rautodiff.py
@@ -1,9 +1,10 @@
 from Dotua.nodes.rscalar import rScalar
 
+
 class rAutoDiff():
     def __init__(self):
-        self.func = None
-        self.universe = []
+        self._func = None
+        self._universe = []
 
     def create_rscalar(self, vals):
         '''
@@ -28,31 +29,31 @@ class rAutoDiff():
             rscalars = [None] * len(vals)
             for i in range(len(vals)):
                 rscalars[i] = rScalar(vals[i])
-            self.universe += rscalars
+            self._universe += rscalars
             return rscalars
         except TypeError:
             rscalar = rScalar(vals)
-            self.universe += [rscalar]
+            self._universe += [rscalar]
             return rscalar
 
     def partial(self, func, var):
         '''
-        Returns the gradient of the function with regarding to this variable
+        Returns derivative of the function with regard to the given variable
 
         INPUTS
         =====
-        func: a function of rscalar variable
-        var: a rscalar variable
+        func: a function of rScalar variables
+        var: an rScalar variable
 
         RETURNS
         =======
         A constant, which is the gradient of func with regarding to var
         '''
-        if (self.func != func):
-            for item in self.universe:
+        if (self._func != func):
+            for item in self._universe:
                 self._reset_universe(item)
             func.grad_val = 1
-            self.func = func
+            self._func = func
         return var.gradient()
 
     def _reset_universe(self, var):
--- Dotua/tests/test_rautodiff.py
+++ Dotua/tests/test_rautodiff.py
@@ -14,13 +14,13 @@ def test_create_rscalar():
     assert x.val == 10
 
 
-def test_reset_universe():
+def test__reset_universe():
     rad = rAutoDiff()
     x, y, z = rad.create_rscalar([1, 3, 6])
     f = x + y - z
     rad.partial(f,z)
 
-    rad.reset_universe(x)
+    rad._reset_universe(x)
     g = op.exp(x)
     assert rad.partial(g, x) == np.exp(1)
 
@@ -51,12 +51,12 @@ def test_partial():
     assert rad.partial(f, y) == 1
     assert rad.partial(f, z) == -1
 
-    # rad.reset_universe([x,y])
+    # rad._reset_universe([x,y])
     g = x * y
     assert rad.partial(g, x) == 3
     assert rad.partial(g, y) == 1
 
-    # rad.reset_universe([x,y,z])
+    # rad._reset_universe([x,y,z])
     h = op.sin(x * y * z) - op.cos(y)
     assert rad.partial(h, x) == 3 * 6 * np.cos(1 * 3 * 6)
     assert rad.partial(h, y) == 1 * 6 * np.cos(1 * 3 * 6) + np.sin(3)
