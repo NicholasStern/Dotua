b9de6aafeaac1cc2efc712e315d455dde2c0ee97
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-12-11 21:26:59 -0500

    Updated test for rvector

--- Dotua/tests/test_rvector.py
+++ Dotua/tests/test_rvector.py
@@ -14,35 +14,35 @@ def test_add():
 	f = x + y
 	g = x + 1
 	h = 3 + x
-	assert ad.partial(f,x) == list([1,1,1])
-	assert ad.partial(g,x) == list([1,1,1])
-	assert ad.partial(h,x) == list([1,1,1])
+	assert list(ad.partial(f,x)) == list([1,1,1])
+	assert list(ad.partial(g,x)) == list([1,1,1])
+	assert list(ad.partial(h,x)) == list([1,1,1])
 
 def test_subtract():
 	f = x - y
 	g = x - 3
 	h = 3 - x
-	assert ad.partial(f,x) == list([1,1,1])
-	assert ad.partial(g,x) == list([1,1,1])
-	assert ad.partial(h,x) == list([-1,-1,-1])
+	assert list(ad.partial(f,x)) == list([1,1,1])
+	assert list(ad.partial(g,x)) == list([1,1,1])
+	assert list(ad.partial(h,x)) == list([-1,-1,-1])
 
 def test_mul():
 	f = x * y
 	g = x * 3
 	h = 3 * x
-	assert ad.partial(f,x) == list(y.val)
-	assert ad.partial(g,x) == list([3,3,3])
-	assert ad.partial(h,x) == list([3,3,3])
+	assert list(ad.partial(f,x)) == list(y.val)
+	assert list(ad.partial(g,x)) == list([3,3,3])
+	assert list(ad.partial(h,x)) == list([3,3,3])
 
 def test_devide():
 	f = x / y
 	g = x / 3
 	h = 3 / x
-	assert ad.partial(f,x) == list(1/y.val)
-	assert ad.partial(g,x) == list([1/3,1/3,1/3])
-	assert ad.partial(h,x) == list(-3/np.power(x,2))
+	assert list(ad.partial(f,x)) == list(1/y.val)
+	assert list(ad.partial(g,x)) == list([1/3,1/3,1/3])
+	assert list(ad.partial(h,x)) == list(-3/np.power(x,2))
 
 def test_neg():
 	f = -x
-	assert ad.partial(f,x) == list([-1,-1,-1])
+	assert list(ad.partial(f,x)) == list([-1,-1,-1])
 
