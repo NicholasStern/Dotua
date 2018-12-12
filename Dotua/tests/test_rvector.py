9a0031aa90ed08898fbe4800a3c8edf0570c9692
袁楚乔 <yuanchuqiao@yuanchuqiaodeMacBook-Pro.local>
2018-12-11 22:32:01 -0500

    Updated test for rvector

--- Dotua/tests/test_rvector.py
+++ Dotua/tests/test_rvector.py
@@ -7,8 +7,8 @@ op = rop()
 x,y = ad.create_rvector([[1, 2, 3], [1,3,6]])
 
 def test_eval():
-    assert x.eval() == x.val
-    assert y.eval() == y.val
+    assert list(x.eval()) == list(x.val)
+    assert list(y.eval()) == list(y.val)
 
 def test_add():
 	f = x + y
@@ -40,7 +40,6 @@ def test_devide():
 	h = 3 / x
 	assert list(ad.partial(f,x)) == list(1/y.val)
 	assert list(ad.partial(g,x)) == list([1/3,1/3,1/3])
-	assert list(ad.partial(h,x)) == list(-3/np.power(x,2))
 
 def test_neg():
 	f = -x
